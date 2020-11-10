645e30b4b042b69b66eda3c3cfc5e93b0a4e470f
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2020-11-10 18:23:40 -0500

    updated javascript code so that each time block get color coded dynamically

--- javascript/planner.js
+++ javascript/planner.js
@@ -1,10 +1,24 @@
 //Variable Declaration
-var todaysDate = moment().format("MMM Do YYYY");
-var todaysTime = moment().format('HH')
-time = moment($('#time1').data('time'),'HH').format('HH')
-
-console.log(time)
-console.log(todaysTime>time)
+var todaysDate = moment().format("dddd MMM Do YYYY");
+var currentTime = parseInt(moment().format('HH'))
 
 
+//function declarations
+function setColor(){
+    for(var i=1;i<10;i++){
+        var textblock = $('#time'+i)
+        textTime = parseInt(moment($('#time'+i).data('time'),'HH').format('HH'))
+        if(textTime>currentTime){
+            $('#time'+i).addClass('future')
+        }else if(textTime<currentTime){
+            $('#time'+i).addClass('past')
+        }else{
+            $('#time'+i).addClass('present')
+        }
+    }
+}
+
+//setting the heading to today's date
 $('#currentDay').text(todaysDate)
+
+setColor();
